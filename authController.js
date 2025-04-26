899e30675d1fe2a64760ba9c0506befc010f36fd
sahil_tiwari <soulsahil200214@gmail.com>
2025-04-26 22:16:14 +0530

    authcontroller

--- authController.js
+++ authController.js
@@ -1,20 +1,27 @@
 const axios = require("axios");
 
-const getInstagramMedia = async (access_token) => {
-    try {
-      const response = await axios.get('https://graph.instagram.com/me/media', {
-        params: {
-          fields: 'id,media_type,media_url,thumbnail_url,permalink',
-          access_token: access_token,
-        },
-      });
-  
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching media:', error);
-      throw error;
+const getInstagramMedia = async (req, res) => {
+    const { access_token } = req.query;
+
+    if (!access_token) {
+        return res.status(400).json({ message: "Access token is required" });
     }
-  };
+
+    try {
+        const response = await axios.get('https://graph.instagram.com/me/media', {
+            params: {
+                fields: 'id,media_type,media_url,thumbnail_url,permalink',
+                access_token: access_token,
+            },
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching media:', error);
+        res.status(500).json({ message: error.response?.data || error.message });
+    }
+};
+
 
 const getInstagramOembed = async (req, res) => {
   const { url, token } = req.query;
